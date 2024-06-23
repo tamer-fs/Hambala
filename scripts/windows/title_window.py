7d54dbfeeb25dfaa0c2a61e40d5ce88fe1ef2f74
tamer-fs <113843535+tamer-fs@users.noreply.github.com>
2024-06-23 18:04:53 +0200

    Co-authored-by: Miny <MineMoebsie@users.noreply.github.com>

--- scripts/windows/title_window.py
+++ scripts/windows/title_window.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_gui
 
+
 pygame.init()
 pygame.font.init()
 
@@ -12,30 +13,36 @@ class TitleWindow:
         self.screen_width, self.screen_height = screen.get_size()
         
         self.manager = pygame_gui.UIManager((self.screen_width, self.screen_height), theme_path="scripts/windows/title_window.json")
-        self.hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((self.screen_width/2-50, self.screen_height/2-25), (100, 50)),
-                                                    text='Print input',
-                                                    manager=self.manager)
+        
+        
+        self.test_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (50, 100)), text="TEST BUTTON", manager=self.manager)
         
     def update(self, events, mouse_x, mouse_y, mouse_down, delta_time): 
         playing = True
         
         self.screen_width, self.screen_height = self.screen.get_size() 
         
+        if events != []:
+            print(events)
         for event in events:
             if event.type == pygame.QUIT:
                 playing = False
-            elif event.type == pygame.VIDEORESIZE:
+                
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                print("TESTBUTTON")
+                
+            if event.type == pygame.VIDEORESIZE:
                 self.manager.set_window_resolution((self.screen_width, self.screen_height))   
-                
-                self.hello_button.set_dimensions((self.screen_width/2, 50))         
-                hello_width = self.hello_button.get_abs_rect().w
-                hello_height = self.hello_button.get_abs_rect().h
-                self.hello_button.set_position((self.screen_width/2-hello_width/2, self.screen_height/2-hello_height/2))
-                
 
+            self.manager.process_events(event)
+                
+                # if event.ui_element == self.test_btn.button:    
+                    
+        # self.test_btn.update(delta_time)
         self.manager.update(delta_time)
         
         return playing
 
     def draw(self):
+        # self.test_btn.draw(self.scre)
         self.manager.draw_ui(self.screen)
